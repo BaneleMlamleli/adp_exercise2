f69b37012cf06cf4485e631f39aaf1fa41ce523a
BaneleMlamleli <mlamlelibanele@yahoo.com>
2019-03-15 21:43:22 +0200

    Added Quadbike class

--- src/main/java/com/adp_exercise2/Bus.java
+++ src/main/java/com/adp_exercise2/Bus.java
@@ -1,13 +1,14 @@
 package com.adp_exercise2;
 
 public class Bus extends Vehicle {
+
     @Override
     public void move() {
-        super.move();
+        System.out.println("Bus is moving very fast");
     }
 
     @Override
     public void wheels() {
-        super.wheels();
+        System.out.println("Bus has eight wheels");
     }
 }
---
+++ src/main/java/com/adp_exercise2/Quadbike.java
@@ -0,0 +1,14 @@
+package com.adp_exercise2;
+
+public class Quadbike extends Vehicle {
+
+    @Override
+    public void move() {
+        System.out.println("Quadbike is moving very fast");
+    }
+
+    @Override
+    public void wheels() {
+        System.out.println("Quadbike has four wheels");
+    }
+}

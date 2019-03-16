56d1b54508d5dd9285f21b2e22f542b0d8a6e62c
BaneleMlamleli <mlamlelibanele@yahoo.com>
2019-03-17 00:00:00 +0200

    Added seven classes for all design principles. No code as yet

--- src/test/java/com/adp_exercise2/core_oop_principles/AppTest.java
+++ src/test/java/com/adp_exercise2/core_oop_principles/AppTest.java
@@ -2,6 +2,8 @@ package com.adp_exercise2.core_oop_principles;
 
 import static org.junit.Assert.assertTrue;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -9,24 +11,27 @@ import org.junit.Test;
  */
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    private static Vehicle vehicle;
+    private static Vehicle bus;
+    private static Vehicle quadbike;
+
+    String busCompanyName = "MyCiti";
+    int maximumSpeed = 160;
+
+    @Before
+    public void setUp() throws Exception {
+        vehicle = new Vehicle();
+        bus = new Bus(busCompanyName);
+        quadbike = new Quadbike(maximumSpeed);
+    }
+
+    @After
+    public void tearDown() throws Exception {
     }
 
     @Test
     public static void main( String[] args )
     {
-        String busCompanyName = "MyCiti";
-        int maximumSpeed = 160;
-        Vehicle vehicle = new Vehicle();
-        Vehicle bus = new Bus(busCompanyName);
-        Vehicle quadbike = new Quadbike(maximumSpeed);
-
         bus.move();
         bus.wheels();
         quadbike.move();
